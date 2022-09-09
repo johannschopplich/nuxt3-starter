ec5516f8de147c88adb4f8d44e9b0d98c974fabe
Johann Schopplich <mail@johannschopplich.com>
2022-09-09 15:12:26 +0200

    refactor: use head composables for meta tags

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
-<script setup lang="ts">
-useHead({
-  title: 'Nuxt 3 Starter',
-})
-</script>
-
 <template>
-  <main class="max-w-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-    <NuxtPage />
-  </main>
+  <Html>
+    <Head>
+      <Title>Nuxt 3 Starter</Title>
+    </Head>
 
-  <AppEnvironment />
+    <Body>
+      <main class="mx-auto max-w-xl py-12 px-4 sm:px-6 lg:px-8">
+        <NuxtPage />
+      </main>
+
+      <AppEnvironment />
+    </Body>
+  </Html>
 </template>
